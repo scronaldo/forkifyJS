ab8d81a1284b5a3f329e46986af446ecb9fc319f
Amazing <naturalnoneedy@gmail.com>
2019-02-18 06:59:35 +0300

    'testing'

--- src/js/config.js
+++ src/js/config.js
@@ -7,4 +7,4 @@ export const key = '96c0f156a634b7a19acd17bd9684e913';
 
 // extra key for this stupid greedy API 96c0f156a634b7a19acd17bd9684e913
 // original key 0029c792d7701ca8334b720d62e17bb3
-// testing git
+// testing git!
