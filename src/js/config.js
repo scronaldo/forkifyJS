bc0c6b24770e155ea9d4794951e8eaf4bf51b08a
Amazing <naturalnoneedy@gmail.com>
2019-03-08 14:29:57 +0300

    'api'

--- src/js/config.js
+++ src/js/config.js
@@ -7,4 +7,8 @@ export const key = '96c0f156a634b7a19acd17bd9684e913';
 
 // extra key for this stupid greedy API 96c0f156a634b7a19acd17bd9684e913
 // original key 0029c792d7701ca8334b720d62e17bb3
-// testing git --- test
+
+
+// extra api heroku 
+// const proxy = 'https://cors-anywhere.herokuapp.com/';
+// export const key = '462b1cc8d4f2730081462fbc65136320';
